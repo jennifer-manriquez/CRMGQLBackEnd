bfc063f1efe7a78ac58e0cfd25c23468e130c41f
Jennifer Manriquez <jennymanb@gmail.com>
2021-11-10 09:32:31 -0600

    Changes to deploy

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ const server = new ApolloServer({
 });
 
 //start server
-server.listen().then( ({url}) => {
+server.listen({port: process.env.PORT || 4000}).then( ({url}) => {
   console.log(`Server ready at URL ${url}`)
 })
